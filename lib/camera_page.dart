90187db4c64e1e2fafba4fa2a47f4f86ae2dbc0f
omar <omarfaruk2bd@gmail.com>
2022-12-12 15:06:38 +0600

    Add contineous text recognization

---
+++ lib/camera_page.dart
@@ -0,0 +1,98 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+
+class CameraPage extends StatefulWidget {
+  CameraPage({
+    Key? key,
+    required this.cameras,
+  }) : super(key: key);
+  List<CameraDescription> cameras;
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  late CameraController controller;
+  XFile? pictureFile;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(widget.cameras[0], ResolutionPreset.max);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return const SizedBox(
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+    return Scaffold(
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: SizedBox(
+                height: 400,
+                width: 400,
+                child: CameraPreview(controller),
+              ),
+            ),
+          ),
+          FutureBuilder(
+            future: getRecognisedText(),
+            builder: ((context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done &&
+                  snapshot.hasError) {
+                return const CircularProgressIndicator();
+              }
+              if (snapshot.hasData) {
+                return Text(snapshot.data!);
+              }
+              return const Text('eror');
+            }),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<String> getRecognisedText() async {
+    pictureFile = await controller.takePicture();
+    if (pictureFile == null) {
+      return 'null...........';
+    } else {
+      final inputImage = InputImage.fromFilePath(pictureFile!.path);
+      final textDetector = GoogleMlKit.vision.textRecognizer();
+      var recognisedText = await textDetector.processImage(inputImage);
+      await textDetector.close();
+      var scannedText = '';
+      for (TextBlock block in recognisedText.blocks) {
+        for (TextLine line in block.lines) {
+          scannedText = "$scannedText${line.text}\n";
+        }
+      }
+      setState(() {});
+      return scannedText;
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:text_recognition_ml/camera_page.dart';
 import 'package:text_recognition_ml/text_recognition.dart';
 
-void main() {
-  runApp(const MyApp());
+late List<CameraDescription> _cameras;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  _cameras = await availableCameras();
+  runApp(MyApp(
+    cameras: _cameras,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({
+    Key? key,
+    required this.cameras,
+  }) : super(key: key);
+  List<CameraDescription> cameras;
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +28,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const TextRecognitionPage(),
+      home: CameraPage(
+        cameras: cameras,
+      ),
     );
   }
 }
